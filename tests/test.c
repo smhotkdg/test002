02a24edd64325a07bcc8b2b01c7bf3720f022235
unknown <cozy0614@gmail.com>
2026-09-10 17:15:13 +0900

    코드 수정

--- tests/test.c
+++ tests/test.c
@@ -4,7 +4,6 @@ void main()
 {
 	int a[7] = { 72,95,48,88,95,61,30 };
 	int max = a[0];        // 임시 챔피언
-	int min = 100;
 	
 
 	for (int i = 1; i < 7; i++)
@@ -14,16 +13,6 @@ void main()
 			max = a[i];
 		}
 	}
-	for (int i = 0; i < 7; i++)
-	{
-		if (a[i] < min)
-		{
-			min = a[i];
-		}
-	}
-		
 			
 	printf("Max Value = %d\n", max);
-	printf("Min Value = %d\n", min);
-
 }
